1b6aa1b0209a4dc4a3e02f25f41d2a5c389b4d5f
Monir <moniru005@gmail.com>
2023-11-15 14:02:50 +0600

    server link change

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://bistro-boss-server-blond-theta.vercel.app/",
 });
 
 const useAxiosSecure = () => {
